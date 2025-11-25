d618fa7beaf5c90fbdb80772daefead8cbc7c9eb
OlgierdCel <olgierdcel07@gmail.com>
2025-11-25 20:12:15 +0100

    dodanie implementacji odnoscie polaczenia z api oraz dzialanie w tle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,10 @@ android {
 
 dependencies {
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
